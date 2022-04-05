f117098d44511955100d0e17e909111892d48b25
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-04-05 11:12:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from application.salary import calculate_salary
 from application.db.people import get_employees
 from datetime import datetime
 
-date_today = datetime.today()
-day_today_formatted = date_today.strftime("%A, %d %B %Y %I:%I%p")
+
+def get_date():
+    date_today = datetime.today()
+    day_today_formatted = date_today.strftime("%A, %d %B %Y %I:%I%p")
+    return day_today_formatted
+
 
 if __name__ == '__main__':
-    print(day_today_formatted)
+    print(get_date())
     application.salary.calculate_salary()
     application.db.people.get_employees()
